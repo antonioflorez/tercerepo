f5917960a2cfcdc73d8b1e2a5c2bb99bb78d9d82
antonio <antonio.florez@aatecnologias.com>
2025-01-29 13:42:49 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from septuptools import setup, find_packages
+
+septup(
+    name = "tercerepo",
+    version = "0.1.0",
+    packages = find_packages(),
+    description = "un paquete pip simple de saludo",
+    author = "antonioflorez",
+    author_email = "antonio.florez@aatecnologias.com",
+    url = "https://gitgithub.com/platzi/git-github",
+)
